9fc9f5d9a3f69c0aede672afca6e559d9312a62e
Ryan Palumbo <82953700+rynpal@users.noreply.github.com>
2022-08-17 15:20:40 -0400

    change pdo connection to environment variable

--- pdo.php
+++ pdo.php
@@ -1,5 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc2','ryan','7[X.KJBbKVNrMRbJ');
-
+$pdo = new PDO(getenv('MYSQLCONNSTR_TRAININGAPP'));
 $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 ?>
